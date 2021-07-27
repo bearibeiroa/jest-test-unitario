d49d0f9d1d2f0786762d574404821d00a19c1d30
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-07-27 21:50:36 +0100

    Requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,21 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
+
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    const products = productDetails('Alcool gel', 'Máscara');
+    assert.strictEqual(Array.isArray(products), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(products.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof products[0] && typeof products[1], 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(products[0], products[1]);
     // Teste que os dois productIds terminam com 123.
+    assert.deepStrictEqual(products[0].details.productId.endsWith('123'), products[1].details.productId.endsWith('123'));
   });
 });
+
+// Referência 1: teste booleano com Array.isArray - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
+// Referência 2 : teste booleano para verificar final de uma string - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
